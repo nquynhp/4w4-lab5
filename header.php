3ac8a4db8c12095e4769f4258d4e2d8cfa148b49
Ngoc Quynh Pham <nquynhp2002@yahoo.ca>
2022-03-17 16:27:50 -0500

    s8-c2 QP Animation du menu principal lorsque je l'ouvre

--- header.php
+++ header.php
@@ -49,8 +49,7 @@
     <section class="site__barre">
         <input type="checkbox" id="chk-burger">
         <label for="chk-burger" id="burger">
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                color="#000">
+        <svg class="svg_menu_principal" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 <path d="M0 0h24v24H0z" fill="none"></path>
                 <path
                     d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
