6034540bbdae902e8bc405e315d4063b662e9be2
Ngoc Quynh Pham <nquynhp2002@yahoo.ca>
2022-05-02 09:24:58 -0400

    s13 c2 nqp - Ajustement des tailles de la galerie

--- front-page.php
+++ front-page.php
@@ -25,7 +25,9 @@
         <?php if (have_posts()): the_post() ?>
 
         <?php the_title();  ?>
+        <div class="galerie">
         <?php the_content(); ?>
+        </div>
         
         <?php endif ?>
 
