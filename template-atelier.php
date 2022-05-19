6fb9c7ff36041ba5feaf7900eb5919fb858fc9f9
Ngoc Quynh Pham <nquynhp2002@yahoo.ca>
2022-05-19 12:44:30 -0400

    ef-nqp - Ajout des champs personnalisés dans template-atelier.php

--- template-atelier.php
+++ template-atelier.php
@@ -11,10 +11,56 @@
 <?php get_header() ?>
 <main class="site__main">
 
-<!-- <h1> --------- Template Annonce --------- </h1> -->
+<h1> --------- Template Atelier --------- </h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
-        <?php the_content() ?>        
+
+        <!-- Description de l'atelier -->
+        <section class="atelier__description">
+            <?php the_field('description'); ?>   
+        </section>
+
+        <!-- L'animateur responsable de l'atelier -->
+        <p class="atelier__animateur">
+        <?php the_field('animateur'); ?>   
+        </p>
+
+        <!-- Date de début de l'atelier -->
+        <p class="atelier__dateDebut">
+        <?php the_field('dateDebut'); ?>   
+        </p>
+
+        <!-- Date de fin de l'atelier -->
+        <p class="atelier__dateFin">
+        <?php the_field('dateFin'); ?>   
+        </p>
+
+        <!-- Les jours de la semaine lorsqu'il y a l'atelier -->
+        <p class="atelier__joursSemaine">
+        <?php the_field('joursSemaine'); ?>   
+        </p>
+
+        <!-- Quand l'atelier débute -->
+        <p class="atelier__heureDebut">
+        <?php the_field('heureDebut'); ?>   
+        </p>
+
+        <!-- Quand l'atelier se termine -->
+        <p class="atelier__heureFin">
+        <?php the_field('heureFin'); ?>   
+        </p>
+
+        <!-- La durée de l'atelier -->
+        <p class="atelier__duree">
+        <?php the_field('duree'); ?>   
+        </p>
+
+        <!-- Local où se déroulera l'atelier -->
+        <p class="atelier__local">
+        <?php the_field('local'); ?>   
+        </p>
+
+        <p><?php the_field('organisateur'); ?></p>      
    <?php endif ?>
 </main>
 <?php get_footer() ?>
